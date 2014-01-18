e5d603e78438600310dee8f1c0f9f273d15d535c
Taiki ONO <taiks.4559@gmail.com>
2014-01-18 17:57:00 +0900

    Add more test

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,9 +9,16 @@ import Data.SimpleHtmlTag
 main :: IO ()
 main = hspec spec
 
+parseTest :: String -> Either String Tag
+parseTest input = (left show $ parseSimpleHtml "Spec" input)
+
 spec :: Spec
 spec = do
         describe "SimpleHtmlParser" $ do
             context "with one tag" $ do
                 it "parses" $ do
-                    (left show $ parseSimpleHtml "Spec" "<html></html>") `shouldBe` (Right $ Tag "html" "" [])
+                    parseTest "<html></html>" `shouldBe` (Right $ Tag "html" "" [])
+            context "with nested tag" $ do
+                it "parses" $ do
+                    parseTest "<html><body></body></html>"
+                        `shouldBe` (Right $ Tag "html" "" [Tag "body" "" [] ])
