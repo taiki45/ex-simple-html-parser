6721fa76abf5c5fdb51ded18c33d721d8a77ae82
Taiki ONO <taiks.4559@gmail.com>
2014-01-18 18:01:42 +0900

    Add more test and TODO

--- test/Spec.hs
+++ test/Spec.hs
@@ -18,7 +18,24 @@ spec = do
             context "with one tag" $ do
                 it "parses" $ do
                     parseTest "<html></html>" `shouldBe` (Right $ Tag "html" "" [])
+            context "with content" $ do
+                it "parses" $ do
+                    parseTest "<html>AAA</html>" `shouldBe` (Right $ Tag "html" "AAA" [])
             context "with nested tag" $ do
                 it "parses" $ do
                     parseTest "<html><body></body></html>"
                         `shouldBe` (Right $ Tag "html" "" [Tag "body" "" [] ])
+            context "with nested tag and content" $ do
+                it "parses" $ do
+                    parseTest "<html><body>AAA</body></html>"
+                        `shouldBe` (Right $ Tag "html" "" [Tag "body" "AAA" [] ])
+            context "with nested contents" $ do
+                it "parses" $ do
+                    parseTest "<html><body>AAA</body>BBB</html>"
+                        `shouldBe` (Right $ Tag "html" "BBB" [Tag "body" "AAA" [] ])
+            context "with invalid nested contents" $ do
+                it "parses" $ do
+                    parseTest "<html>CCC<body>AAA</body>BBB</html>"
+                        `shouldBe` (Left "\"Spec\" (line 1, column 10):\nunexpected \"b\"\nexpecting \"</\"")
+                        -- TODO: pass this case with:
+                        --   `shouldBe` (Right $ Tag "html" "CCCBBB" [Tag "body" "AAA" [] ])
